2de69b9845a8161aefbc714ad25024504eed7c67
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2025-04-28 17:51:08 +0530

    Merging code from Base branch to SDK

---
+++ Coditech.Project/Coditech.Engine.Admin/wwwroot/js/Coditech/Organisation/OrganisationCentrewisePolicy.js
@@ -0,0 +1,56 @@
+﻿var OrganisationCentrewisePolicy = {
+    Initialize: function () {
+        OrganisationCentrewisePolicy.constructor();
+    },
+    constructor: function () {
+    },
+    LoadListByCentreCode: function (controllerName, methodName) {
+        var centreCode = $("#CentreCode").val();
+        if (centreCode === "") {
+            CoditechNotification.DisplayNotificationMessage("Please select centre.", "error");
+        } else {
+            $.ajax({
+                type: "GET",
+                url: "/" + controllerName + "/" + methodName,
+                data: { centreCode: centreCode },
+                success: function (result) {
+                    $("#DataTablesDivId").html(result);
+                },
+                error: function () {
+                    CoditechNotification.DisplayNotificationMessage("Failed to load list.", "error");
+                }
+            });
+        }
+    },
+    GetCentrewisePolicyDetails: function (modelPopContentId, generalPolicyRulesId) {
+        var centreCode = $("#CentreCode").val();
+        var centreName = $("#CentreCode option:selected").text();
+        CoditechCommon.ShowLodder();
+        $.ajax(
+            {
+                cache: false,
+                type: "GET",
+                dataType: "html",
+                url: "/OrganisationCentrewisePolicy/GetCentrewisePolicyDetails",
+                data: {
+                    centreCode: centreCode,
+                    generalPolicyRulesId: generalPolicyRulesId
+                },
+                contentType: "application/json; charset=utf-8",
+                success: function (result) {
+                    $('#' + modelPopContentId).html("").html(result);
+                    CoditechCommon.HideLodder();
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    if (xhr.status == "401" || xhr.status == "403") {
+                        location.reload();
+                    }
+                    CoditechNotification.DisplayNotificationMessage("Failed to display record.", "error");
+                    CoditechCommon.HideLodder();
+                }
+            });
+    },
+    CentrewisePolicy: function () {
+        $("#frmCentrewisePolicy").submit();
+    },
+}
