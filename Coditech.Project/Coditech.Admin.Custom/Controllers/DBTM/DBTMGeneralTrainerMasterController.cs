f5daed1513824c862f4cb1392eebb786bf891bc4
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2025-07-24 20:13:22 +0530

    Code check in for trainer add

---
+++ Coditech.Project/Coditech.Admin.Custom/Controllers/DBTM/DBTMGeneralTrainerMasterController.cs
@@ -0,0 +1,61 @@
+﻿using Coditech.Admin.Agents;
+using Coditech.Admin.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Coditech.Admin.Controllers
+{
+    public class DBTMGeneralTrainerMasterController : BaseController
+    {
+        private readonly IDBTMNewRegistrationAgent _dBTMNewRegistrationAgent;
+
+        public DBTMGeneralTrainerMasterController(IDBTMNewRegistrationAgent dBTMNewRegistrationAgent)
+        {
+            _dBTMNewRegistrationAgent = dBTMNewRegistrationAgent;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public  ActionResult TrainerRegistration()
+        {
+            TempData["FormSizeClass"] = "col-lg-8";
+            return View("~/Views/DBTM/DBTMGeneralTrainerMaster/DBTMTrainerRegistration.cshtml", new DBTMNewRegistrationViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public  ActionResult TrainerRegistration(DBTMNewRegistrationViewModel dBTMNewRegistrationViewModel)
+        {
+            TempData["FormSizeClass"] = "col-lg-8";
+
+            if (!dBTMNewRegistrationViewModel.IsTermsAndCondition)
+            {
+                dBTMNewRegistrationViewModel.ErrorMessage = "Please accept Terms And Conditions.";
+            }
+            else
+            {
+                ModelState.Remove("DeviceSerialCode");
+                ModelState.Remove("CentreName");
+                ModelState.Remove("DateOfBirth");
+                ModelState.Remove("JoiningCode");
+                ModelState.Remove("SpecializationEnumId");
+                ModelState.Remove("SelectedTrainer");
+                if (ModelState.IsValid)
+                {
+                    dBTMNewRegistrationViewModel = _dBTMNewRegistrationAgent.TrainerRegistration(dBTMNewRegistrationViewModel);
+                    if (!dBTMNewRegistrationViewModel.HasError)
+                    {
+                        TempData["FormSizeClass"] = "col-lg-4";
+                        SetNotificationMessage(GetSuccessNotificationMessage("Your Registration successfully."));
+                        return RedirectToAction("List", "GeneralTrainerMaster");
+                    }
+                }
+            }
+            SetNotificationMessage(GetErrorNotificationMessage(dBTMNewRegistrationViewModel.ErrorMessage));
+            return View("~/Views/DBTM/DBTMGeneralTrainerMaster/DBTMTrainerRegistration.cshtml", dBTMNewRegistrationViewModel);
+        }
+    }
+}
+
+
