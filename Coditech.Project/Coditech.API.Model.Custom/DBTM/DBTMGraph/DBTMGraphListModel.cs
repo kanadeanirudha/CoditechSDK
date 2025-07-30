b8de466a02bd1dc9ead0fc79ae7a3a84ab3e74a5
Anirudha Kanade <anirudhakanade1007@gmail.com>
2025-07-30 15:09:54 +0530

    code correction

--- Coditech.Project/Coditech.API.Model.Custom/DBTM/DBTMGraph/DBTMGraphListModel.cs
+++ Coditech.Project/Coditech.API.Model.Custom/DBTM/DBTMGraph/DBTMGraphListModel.cs
@@ -1,13 +1,8 @@
-﻿using System.Data;
-
-namespace Coditech.Common.API.Model
+﻿namespace Coditech.Common.API.Model
 {
     public class DBTMGraphListModel : BaseListModel
     {
-        public DataTable DataTable { get; set; }
-        public DBTMGraphListModel()
-        {
-            DataTable = new DataTable();
-        }
+        public DBTMReportsListModel dbtmReportsListModel { get; set; }
+        public DBTMTestModel dbtmTestModel { get; set; }
     }
 }
--- Coditech.Project/Coditech.Engine.DBTM/Service/Implementation/DBTMReportsService.cs
+++ Coditech.Project/Coditech.Engine.DBTM/Service/Implementation/DBTMReportsService.cs
@@ -51,8 +51,25 @@ namespace Coditech.API.Service
             return BindDBTMDataDetails(dBTMTestMasterId, isMobileRequest, dBTMReportsList);
         }
 
-   
+
         public DBTMReportsListModel TestWiseReports(int dBTMTestMasterId, long dBTMTraineeDetailId, DateTime fromDate, DateTime toDate, long entityId, string userType, string centreCode, bool isMobileRequest)
+        {
+            return GetTestWiseReports(dBTMTestMasterId, dBTMTraineeDetailId, fromDate, toDate, entityId, userType, centreCode, isMobileRequest);
+        }
+
+        public DBTMGraphListModel TestWiseGraphReports(int dBTMTestMasterId, long dBTMTraineeDetailId, DateTime fromDate, DateTime toDate, long entityId, string userType, string centreCode, bool isMobileRequest)
+        {
+            DBTMReportsListModel DBTMReportsListModel = GetTestWiseReports(dBTMTestMasterId, dBTMTraineeDetailId, fromDate, toDate, entityId, userType, centreCode, isMobileRequest);
+            DBTMTestMaster dBTMTestMaster = _dBTMTestMasterRepository.Table.Where(x => x.DBTMTestMasterId == dBTMTestMasterId)?.FirstOrDefault();
+            DBTMGraphListModel graphListModel = new DBTMGraphListModel()
+            {
+                dbtmReportsListModel = DBTMReportsListModel,
+                dbtmTestModel = dBTMTestMaster?.FromEntityToModel<DBTMTestModel>()
+            };
+            return graphListModel;
+        }
+
+        private DBTMReportsListModel GetTestWiseReports(int dBTMTestMasterId, long dBTMTraineeDetailId, DateTime fromDate, DateTime toDate, long entityId, string userType, string centreCode, bool isMobileRequest)
         {
             if (dBTMTestMasterId <= 0)
             {
@@ -77,36 +94,7 @@ namespace Coditech.API.Service
             return BindDBTMDataDetails(dBTMTestMasterId, isMobileRequest, dBTMReportsList);
         }
 
-        public DBTMGraphListModel TestWiseGraphReports(int dBTMTestMasterId, long dBTMTraineeDetailId, DateTime fromDate, DateTime toDate, long entityId, string userType, string centreCode, bool isMobileRequest)
-        {
-            if (dBTMTestMasterId <= 0)
-            {
-                return new DBTMGraphListModel();
-            }
-            if (userType == UserTypeEnum.Employee.ToString())
-            {
-                entityId = 0;
-            }
-            //Bind the Filter, sorts & Paging details.
-            PageListModel pageListModel = new PageListModel(null, null, 0, 0);
-            CoditechViewRepository<DBTMReportsModel> objStoredProc = new CoditechViewRepository<DBTMReportsModel>(_serviceProvider.GetService<CoditechCustom_Entities>());
-            objStoredProc.SetParameter("@DBTMTestMasterId", dBTMTestMasterId, ParameterDirection.Input, DbType.Int32);
-            objStoredProc.SetParameter("@DBTMTraineeDetailId", dBTMTraineeDetailId, ParameterDirection.Input, DbType.Int64);
-            objStoredProc.SetParameter("@FromDate", fromDate, ParameterDirection.Input, DbType.Date);
-            objStoredProc.SetParameter("@ToDate", toDate, ParameterDirection.Input, DbType.Date);
-            objStoredProc.SetParameter("@GeneralTrainerMasterId", entityId, ParameterDirection.Input, DbType.Int64);
-            objStoredProc.SetParameter("@CentreCode", centreCode, ParameterDirection.Input, DbType.String);
-            objStoredProc.SetParameter("@RowsCount", pageListModel.TotalRowCount, ParameterDirection.Output, DbType.Int32);
-            List<DBTMReportsModel> dBTMReportsList = objStoredProc.ExecuteStoredProcedureList("Coditech_GetDBTMTestWiseReportsList @DBTMTestMasterId,@DBTMTraineeDetailId,@FromDate,@ToDate,@GeneralTrainerMasterId,@CentreCode,@RowsCount OUT", 6, out pageListModel.TotalRowCount)?.ToList();
-            DBTMReportsListModel reportsModel = BindDBTMDataDetails(dBTMTestMasterId, isMobileRequest, dBTMReportsList);
-            DBTMGraphListModel graphListModel = new DBTMGraphListModel()
-            {
-               
-            };
-            return graphListModel;
-        }
-
-        public DBTMReportsListModel BindDBTMDataDetails(int dBTMTestMasterId, bool isMobileRequest, List<DBTMReportsModel> dBTMReportsList)
+        private DBTMReportsListModel BindDBTMDataDetails(int dBTMTestMasterId, bool isMobileRequest, List<DBTMReportsModel> dBTMReportsList)
         {
             DBTMReportsListModel listModel = new DBTMReportsListModel();
 
