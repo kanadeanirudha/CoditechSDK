88f304ac2395f3dd1143dad523d4712f496a4686
Anirudha Kanade <anirudhakanade1007@gmail.com>
2025-10-20 18:38:33 +0530

    code correction

---
+++ Coditech.Project/Coditech.API.Data.Custom/DataModel/DBTM/DBTMTestParameterListviewSequence.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Coditech.API.Data
+{
+    public partial class DBTMTestParameterListviewSequence
+    {
+        [Key]
+        public long DBTMTestParameterListviewSequenceId { get; set; }
+        public string TestCode { get; set; }
+        public string ParameterCode { get; set; }
+        public bool IsCalculatedParameter { get; set; }
+        public Int16 Recursion { get; set; }
+        public Int16 SequenceNumber { get; set; }
+        public string ConsecutiveParameterCode { get; set; }
+        public bool IsCalculatedConsecutiveParameterCode { get; set; }
+        public Nullable<long> CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<long> ModifiedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+    }
+}
+
