3254810b3f80296cba1a72303c7d80e159a0ab13
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2025-12-23 18:09:28 +0530

    Code check in for date added highlighted in calendar

--- Coditech.Project/Coditech.API.Client.Custom/Client/Interface/DBTM/IDBTMReportsClient.cs
+++ Coditech.Project/Coditech.API.Client.Custom/Client/Interface/DBTM/IDBTMReportsClient.cs
@@ -13,5 +13,6 @@ namespace Coditech.API.Client
         DBTMTestWiseReportsListResponse TestWiseMultipleReportsFile(string dBTMTestMasterIds, long dBTMTraineeDetailId, DateTime fromDate, DateTime toDate, long entityId, string userType, string centreCode, string reportType);
         DBTMTestWiseReportsListResponse BatchWiseMultipleReportsFile(string dBTMTestMasterIds, int generalBatchMasterId, DateTime fromDate, DateTime toDate, long entityId, string userType, string centreCode, string reportType);
         TrueFalseResponse DeleteReportsFile(ParameterModel body);
+        List<string> GetActivityPerformedDates(int dBTMTestMasterId, long dBTMTraineeDetailId);
     }
 }
--- Coditech.Project/Coditech.Engine.DBTM/Service/Implementation/DBTMReportsService.cs
+++ Coditech.Project/Coditech.Engine.DBTM/Service/Implementation/DBTMReportsService.cs
@@ -25,6 +25,7 @@ namespace Coditech.API.Service
         private readonly ICoditechRepository<DBTMGraphMaster> _dBTMGraphMasterRepository;
         private readonly ICoditechRepository<GeneralBatchMaster> _generalBatchMasterRepository;
         private readonly ICoditechRepository<DBTMTestParameterListViewSequence> _dBTMTestParameterListviewSequenceRepository;
+        private readonly ICoditechRepository<DBTMTraineeDetails> _dBTMTraineeDetailsRepository;
         public DBTMReportsService(ICoditechLogging coditechLogging, IServiceProvider serviceProvider) : base(serviceProvider)
         {
             _serviceProvider = serviceProvider;
@@ -39,6 +40,7 @@ namespace Coditech.API.Service
             _dBTMCalculationAssociatedToTestRepository = new CoditechRepository<DBTMCalculationAssociatedToTest>(_serviceProvider.GetService<CoditechCustom_Entities>());
             _dBTMParametersAssociatedToTestRepository = new CoditechRepository<DBTMParametersAssociatedToTest>(_serviceProvider.GetService<CoditechCustom_Entities>());
             _dBTMTestParameterListviewSequenceRepository = new CoditechRepository<DBTMTestParameterListViewSequence>(_serviceProvider.GetService<CoditechCustom_Entities>());
+            _dBTMTraineeDetailsRepository = new CoditechRepository<DBTMTraineeDetails>(_serviceProvider.GetService<CoditechCustom_Entities>());
         }
 
         #region Graph
@@ -147,6 +149,23 @@ namespace Coditech.API.Service
                 }
             }
             return graphModel;
+        }
+
+        public List<DateTime> GetActivityPerformedDates(int dBTMTestMasterId, long dBTMTraineeDetailId)
+        {
+            if (dBTMTestMasterId <= 0 || dBTMTraineeDetailId <= 0)
+                return new List<DateTime>();
+
+            var dates = (from dd in _dBTMDeviceDataRepository.Table
+                         join td in _dBTMTraineeDetailsRepository.Table
+                             on dd.PersonCode equals td.PersonCode
+                         join tm in _dBTMTestMasterRepository.Table
+                             on dd.TestCode equals tm.TestCode
+                         where td.DBTMTraineeDetailId == dBTMTraineeDetailId
+                               && tm.DBTMTestMasterId == dBTMTestMasterId
+                         select (dd.CreatedDate ?? dd.TestPerformedTime).Date).Distinct().OrderBy(d => d).ToList();
+
+            return dates;
         }
 
         private void BindInstantaneousChart(GraphModel graphModel, DBTMGraphMaster graphMaster, string yParameter, List<DBTMReportsModel> dBTMReportsList, int colorIndex, IEnumerable<IGrouping<DateTime, DBTMReportsModel>> groupedReports, string[] colorPalette)
