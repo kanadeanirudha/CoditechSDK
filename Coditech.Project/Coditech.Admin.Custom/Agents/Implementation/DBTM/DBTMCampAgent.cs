ce70678f3baf37550423e328aa83c07993d8488e
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2026-04-01 12:07:08 +0530

    Code check in for camp is active colomn added in list

--- Coditech.Project/Coditech.Admin.Custom/Agents/Implementation/DBTM/DBTMCampAgent.cs
+++ Coditech.Project/Coditech.Admin.Custom/Agents/Implementation/DBTM/DBTMCampAgent.cs
@@ -244,6 +244,12 @@ namespace Coditech.Admin.Agents
                 ColumnCode = "CampEndDate",
                 IsSortable = true,
             });
+            datatableColumnList.Add(new DatatableColumns()
+            {
+                ColumnName = "Is Active",
+                ColumnCode = "IsActive",
+                IsSortable = true
+            });
             return datatableColumnList;
         }
         protected virtual List<DatatableColumns> BindAssociatedBatchColumns()
