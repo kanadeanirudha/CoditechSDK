242be57974985f865462bb226475e62dd90900a7
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2026-04-21 18:21:41 +0530

    code check in for TestCalculationOutputId added in DBTMTestMaster

--- Coditech.Project/Coditech.API.Data.Custom/DataModel/DBTM/DBTMTestMaster.cs
+++ Coditech.Project/Coditech.API.Data.Custom/DataModel/DBTM/DBTMTestMaster.cs
@@ -18,6 +18,7 @@ namespace Coditech.API.Data
         public long? TestMediaId { get; set; }
         public bool IsActive { get; set; }
         public bool IsStartDirection { get; set; }
+        public byte TestCalculationOutputId { get; set; }
         public Nullable<long> CreatedBy { get; set; }
         public Nullable<System.DateTime> CreatedDate { get; set; }
         public Nullable<long> ModifiedBy { get; set; }
