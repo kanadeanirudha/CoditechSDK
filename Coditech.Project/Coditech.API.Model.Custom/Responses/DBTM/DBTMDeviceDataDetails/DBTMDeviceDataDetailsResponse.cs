01a239eaac39c21e030190a5a52f04a9843f91b6
Gajendra Pardhi <gajjupardhi58@gmail.com>
2026-05-09 11:26:08 +0530

    Code check in for parametrvalued decryption form

---
+++ Coditech.Project/Coditech.API.Model.Custom/Responses/DBTM/DBTMDeviceDataDetails/DBTMDeviceDataDetailsResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Coditech.Common.API.Model.Responses
+{
+    public class DBTMDeviceDataDetailsResponse : BaseResponse
+    {
+        public DBTMDeviceDataDetailsModel DBTMDeviceDataDetailsModel { get; set; }
+    }
+}
+
---
+++ Coditech.Project/Coditech.Engine.DBTM/Service/Interface/IDBTMGeneralCommonService.cs
@@ -0,0 +1,8 @@
+﻿using Coditech.Common.API.Model;
+namespace Coditech.API.Service
+{
+    public interface IDBTMGeneralCommonService
+    {
+        DBTMDeviceDataDetailsModel GetDBTMDeviceDataDecrypted(string dBTMDeviceDataIds);
+    }
+}
