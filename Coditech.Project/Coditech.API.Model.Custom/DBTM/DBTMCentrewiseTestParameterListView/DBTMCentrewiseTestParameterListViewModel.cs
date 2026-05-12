3fc3c85fc7f4f137a71189f0e5e309725b7e9c6b
Anirudha Kanade <anirudhakanade1007@gmail.com>
2026-05-12 18:09:57 +0530

    code correction

--- Coditech.Project/Coditech.API.Model.Custom/DBTM/DBTMCentrewiseTestParameterListView/DBTMCentrewiseTestParameterListViewModel.cs
+++ Coditech.Project/Coditech.API.Model.Custom/DBTM/DBTMCentrewiseTestParameterListView/DBTMCentrewiseTestParameterListViewModel.cs
@@ -8,6 +8,7 @@
         public string ParameterCode { get; set; }
         public short SequenceNumber { get; set; }
         public string ColumnName { get; set; }
+        public string ColumnDisplayName { get; set; }
         public List<DBTMCentrewiseTestParameterListViewModel> DBTMCentrewiseTestParameterList { get; set; }
         public string DisplayOn { get; set; }
         public bool IsColumnCellBold { get; set; }
