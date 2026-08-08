04904cef86389fd83a154c56ad108dac198192da
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2026-08-08 15:47:23 +0530

    code correction for NumberOfActivityPerformed sorting resolved

--- Coditech.Project/Coditech.Admin.Custom/Agents/Implementation/DBTM/DBTMTraineeDetailsAgent.cs
+++ Coditech.Project/Coditech.Admin.Custom/Agents/Implementation/DBTM/DBTMTraineeDetailsAgent.cs
@@ -582,6 +582,7 @@ namespace Coditech.Admin.Agents
             {
                 ColumnName = "Number of Activity Performed",
                 ColumnCode = "NumberOfActivityPerformed",
+                IsSortable = true,
             });
             datatableColumnList.Add(new DatatableColumns()
             {
