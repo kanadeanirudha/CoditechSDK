6737b42b561caf4f1ead5d22882a208d90f832d8
Nikita Ghansawant <nikitacoditech8149@gmail.com>
2026-08-29 16:03:20 +0530

    Code correction For DBTMGeneralBatchMaster

---
+++ Coditech.Project/Coditech.API.Model.Custom/DBTM/DBTMGeneralBatchMaster/DBTMGeneralBatchMasterListModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Coditech.Common.API.Model
+{
+    public class DBTMGeneralBatchMasterListModel : BaseListModel
+    {
+        public List<DBTMGeneralBatchMasterModel> DBTMGeneralBatchMasterList { get; set; }
+        public DBTMGeneralBatchMasterListModel()
+        {
+            DBTMGeneralBatchMasterList = new List<DBTMGeneralBatchMasterModel>();
+        }
+    }
+}
